a954ba4ef457de5d86398e222d19f086e334eafe
Mike Nowak <mikenowak@users.noreply.github.com>
2018-01-10 16:35:43 +0900

    Replaced string with boolean where tested for dismissed alarms in check_alerts()

--- check_freenas.py
+++ check_freenas.py
@@ -100,7 +100,7 @@ class Startup(object):
         msg=''
         try:
             for alert in alerts:
-              if alert['dismissed'] == 'false':
+              if alert['dismissed'] == False:
                 if alert['level'] == 'CRIT':
                     crit = crit + 1
                     msg = msg + '- (C) ' + string.replace(alert['message'], '\n', '. ') + ' '
